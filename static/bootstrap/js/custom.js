69cac8a10d1f93debda2087b73fabb38f20cd769
İrem DEMİRÖZ <irem.demiroz@ekinoks.io>
2023-09-12 11:07:34 +0300

    Modal formların submit durumlarının kontrolü için custom.js dosyasına fonskiyon ve click event eklenmistir.

--- static/bootstrap/js/custom.js
+++ static/bootstrap/js/custom.js
@@ -11,6 +11,34 @@ $(function () {
             },
             success: function (data) {
                 $("#modal-book .modal-content").html(data.html_form);
+            },
+            error: function(data){
+                closeModal();
+            }
+        });
+    };
+
+   var saveForm = function (e) {
+        e.preventDefault();
+        var form = $(this);
+        var $popup = $("#modal-book");
+        $.ajax({
+          url: form.attr("action"),
+          data: form.serialize(),
+          type: form.attr("method"),
+          cache: false,
+          dataType: 'json',
+          success: function (data) {
+            if (data.form_is_valid) {
+              $("#book-table tbody").html(data.html_book_list);
+              closeModal();
+            }
+            else {
+              $("#modal-book .modal-content").html(data.html_form);
+            }
+          },
+          error: function(data){
+            closeModal();
           }
         });
     };
@@ -23,8 +51,10 @@ $(function () {
     $(".close-modal").click(closeModal)
     // Create book
     $(".add-book").click(loadForm);
+    $("#modal-book").on("submit", ".add-book-form", saveForm);
     // Update book
     $("#book-table").on("click", ".edit-book", loadForm);
+    $("#modal-book").on("submit", ".edit-book-form", saveForm);
     // Delete book
     $("#book-table").on("click", ".delete-book", loadForm);
 });
